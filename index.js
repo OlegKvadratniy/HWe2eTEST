89df5919d1d5404ad5d3e80df2ca15fa2c6b0e20
OlegKvadratniy <sacha.margelov@gmail.com>
2026-06-13 13:27:15 +0300

    test

--- index.js
+++
@@ -1,8 +0,0 @@
-async function run() {
-  const chalk = (await import('chalk')).default;
-  console.log(chalk.blue('Hello, npm!'));
-  console.log(chalk.red.bold('This is an important message.'));
-  console.log(chalk.green('Success!'));
-}
-
-run();
